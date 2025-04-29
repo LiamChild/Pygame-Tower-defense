fe00186fed474fd9669b1fe7a817dc780c8acae2
Liam Child <chi001@stpiusx.nsw.edu.au>
2025-04-29 13:59:29 +1000

    Initial Load

---
+++ enemy.py
@@ -0,0 +1,48 @@
+import pygame as pg
+from pygame.math import Vector2
+import math
+
+class Enemy(pg.sprite.Sprite):
+    def __init__(self, waypoints, image):
+        pg.sprite.Sprite.__init__(self)
+        self.waypoints = waypoints
+        self.pos = Vector2(self.waypoints[0])
+        self.target_waypoint = 1
+        self.speed = 2
+        self.angle = 0
+        self.image = image
+        self.original_image = image
+        self.image = pg.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        
+    def update(self):
+        self.move() 
+        self.rotate()  # Ensure this can be called
+
+    def move(self):
+        # Define target waypoint
+        if self.target_waypoint < len(self.waypoints):
+            self.target = Vector2(self.waypoints[self.target_waypoint])
+            self.movement = self.target - self.pos
+        else:
+            # Enemy has reached the end of the path
+            self.kill()
+
+        # Calculate distance to target
+        dist = self.movement.length()
+        # Check if remaining distance is greater than the enemy speed
+        if dist >= self.speed:
+            self.pos += self.movement.normalize() * self.speed
+        else: 
+            if dist != 0:
+                self.pos += self.movement.normalize() * dist
+            self.target_waypoint += 1  
+
+    def rotate(self):
+        """Rotates the enemy sprite to face the next waypoint."""
+        if self.target_waypoint < len(self.waypoints):
+            dist = self.target - self.pos
+            self.angle = math.degrees(math.atan2(-dist[1], dist[0]))
+            self.image = pg.transform.rotate(self.original_image, self.angle)
+            self.rect = self.image.get_rect(center=self.pos)
